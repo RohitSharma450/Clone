9a7aa68896056444cffe3cf9968329f185a6d9f7
RohitSharma450 <mern.rohitsharma1@gmail.com>
2025-11-15 11:06:23 +0530

    Add utils

---
+++ Backend/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+const corsOption = {
+    origin: process.env.FRONTEND_URL,  
+    credentials: true           
+};
+
+app.use(cors(corsOption));
+app.use(express.json({limit: "16kb"}))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.status(202).json({message: "Server is runing."})
+})
+
+export { app }
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
-import express from "express";
+import { app } from "./app.js";
 import Main from "./config/db.js";
 
 dotenv.config();
-Main();
-
-const app = express();
+Main()
+.catch((err) => {
+  console.log("DB Failed", err)
+  process.exit(1)}
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 PORT IS LISTING : http://localhost:${process.env.PORT}`);
