540fb0978e5327e28fd0959a75adc5d293506fb3
RohitSharma450 <mern.rohitsharma1@gmail.com>
2025-11-20 09:01:04 +0530

    add multer and cloudinary

---
+++ Backend/src/middleware/multer.middlware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+import { storage } from "../utils/cloudinary.js";
+
+// Multer configuration for Cloudinary
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10 MB limit
+  },
+  fileFilter(_, file, cb) {
+    const allowedMimes = ["image/jpeg", "image/png", "image/jpg"];
+    if (allowedMimes.includes(file.mimetype)) {
+      return cb(null, true);
+    }
+    cb(new Error("Invalid file type. Only jpg, jpeg, and png are allowed."));
+  },
+});
+
+export { upload };
